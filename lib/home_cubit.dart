d5df0e0e06aed447e2b147b84b4cfcf74228e4da
Abbos Donaboyev <donaboyevabbos1701@gmail.com>
2022-03-01 17:53:47 +0500

    swiping with loading is working

--- lib/home_cubit.dart
+++ lib/home_cubit.dart
@@ -2,8 +2,19 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'home_state.dart';
+
 part 'home_cubit.freezed.dart';
 
 class HomeCubit extends Cubit<HomeState> {
-  HomeCubit() :super(const HomeState());
+  HomeCubit() : super(const HomeState());
+
+  Future<void> makeConfirm() async {
+    emit(state.copyWith(isLoading: true));
+    Future.delayed(
+      const Duration(seconds: 3),
+      () {
+        emit(state.copyWith(isLoading: false));
+      },
+    );
+  }
 }
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,13 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'swipebuttonflutter.dart';
+import 'home_cubit.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Hello world'),
+    final HomeCubit cubit = HomeCubit();
+    return BlocProvider(
+      create: (_) => cubit,
+      child: BlocBuilder<HomeCubit, HomeState>(
+        builder: (context, state) => Scaffold(
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SwipingButton(
+                isLoading: state.isLoading,
+                text: 'Swipe to finish',
+                onSwipeCallback: () {
+                  debugPrint('======> on swipe callback');
+                  cubit.makeConfirm();
+                },
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
